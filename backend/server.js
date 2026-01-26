5247b1274fec1b17463f39d3394aafffb079f56f
Anos714 <rahul_sain0@outlook.com>
2026-01-26 20:32:41 +0530

    updated cors/server.js file

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://codevault-olive.vercel.app/"],
+    origin: ["http://localhost:5173", "https://codevault-olive.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
